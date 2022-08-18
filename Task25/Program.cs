8f9e8bd37bd472602cfd28ae2007856b68fb9999
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-18 22:32:21 +0500

    Task 25 is done

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -3,3 +3,22 @@
 
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+Console.Write("Enter the number A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the number B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int AtoBDegree(int ac, int bc)
+{
+    int result = 1;
+    for (int i = 0; i <= bc; i++)
+    {
+        result = result * ac;
+    }
+    return result;
+}
+
+int aToBDegree = AtoBDegree(a, b);
+
+Console.WriteLine($"{a} to the power of {b} equals {aToBDegree}.");
