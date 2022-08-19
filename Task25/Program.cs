36cf72726735c6b2fb78606f34dea7122be05907
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-19 09:10:51 +0500

    hw task 25 optimized

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -12,7 +12,7 @@ int b = Convert.ToInt32(Console.ReadLine());
 int AtoBDegree(int ac, int bc)
 {
     int result = 1;
-    for (int i = 0; i <= bc; i++)
+    for (int i = 0; i < bc; i++)
     {
         result = result * ac;
     }
